c8504a6f0daa8e618c33e0451e3d2956ef33a9c5
Math55-99 <darosamatheus05@gmail.com>
2026-08-10 11:50:22 -0300

    Add files via upload

---
+++ 03-07/meuSegundoArquivo.js
@@ -0,0 +1,11 @@
+let firstName = "Matheus"
+let age = 20
+let ofLegalAge = 18
+let yearOfBirth = 2006
+let futureYears = 2050
+
+console.log("Nome:", firstName)
+console.log("Idade:", age)
+console.log("É maior de idade?", age >= ofLegalAge)
+console.log("Idade em 2050:", futureYears - yearOfBirth)
+ 
