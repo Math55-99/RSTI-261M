d35ded520a80b796d40f508cd48658faf04158f5
Math55-99 <darosamatheus05@gmail.com>
2026-08-10 11:51:06 -0300

    Add files via upload

---
+++ 06-07/meuTerceiroArquivo.js
@@ -0,0 +1,11 @@
+let num5 = 6
+let num6 = 7
+console.clear()
+
+if (num5 > num6) {
+    console.log(num5, "é maior que", num6)
+} else if (num5 < num6) {
+    console.log(num5, "é menor que", num6)
+} else {
+    console.log(num5, "é igual a", num6)
+}
