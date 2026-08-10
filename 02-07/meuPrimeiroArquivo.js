44bebf8f953d105eeba72999603e6bd9ba23469d
Math55-99 <darosamatheus05@gmail.com>
2026-08-10 11:49:57 -0300

    Add files via upload

---
+++ 02-07/meuPrimeiroArquivo.js
@@ -0,0 +1,28 @@
+let firstNumber = 6
+let secondNumber = 4
+let thirdNumber = "6"
+let fourthNumber = "4"
+let fifthNumber = 6
+let sixthNumber = "4"
+let seventhNumber = "6"
+let eighthNumber = 4
+
+console.log( "The fistnumber is even the secondnumber?", firstNumber === secondNumber, "True/False")
+console.log( "The fistnumber is different from the secondnumber?", firstNumber !== secondNumber, "True/False")
+console.log( "The fistnumber is greater than the secondnumber?", firstNumber > secondNumber, "True/False")
+console.log( "The fistnumber is less than the secondnumber?", firstNumber < secondNumber, "True/False")
+
+console.log( "The thirdnumber is even to the fourthnumber?", thirdNumber === fourthNumber, "True/False")
+console.log( "The thirdnumber is different from the fourthnumber?", thirdNumber !== fourthNumber, "True/False")
+console.log( "The thirdnumber is greater than the fourthnumber?", thirdNumber > fourthNumber, "True/False")
+console.log( "The thirdnumber is less than the fourthnumber?", thirdNumber < fourthNumber, "True/False")
+
+console.log( "The fifthnumber is even to the sixthnumber?", fifthNumber === sixthNumber, "True/False")
+console.log( "The fifthnumber is different from the sixthnumber?", fifthNumber !== sixthNumber, "True/False")
+console.log( "The fifthnumber is greater than the sixthnumber?", fifthNumber > sixthNumber, "True/False")
+console.log( "The fifthnumber is less than the sixthnumber?", fifthNumber < sixthNumber, "True/False")
+
+console.log( "The seventhnumber is even to the eighthnumber?", seventhNumber === eighthNumber, "True/False")
+console.log( "The seventhnumber is different from the eighthnumber?", seventhNumber !== eighthNumber, "True/False")
+console.log( "The seventhnumber is greater than the eighthnumber?", seventhNumber > eighthNumber, "True/False")
+console.log( "The seventhnumber is less than the eighthnumber?", seventhNumber < eighthNumber, "True/False")
